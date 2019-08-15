b0a3106634a369fea8acbad508038bbface681d3
rimobile1 <icaro.mo1234@gmail.com>
2019-08-14 22:13:05 -0300

    Valor padrão (função)

--- dist/revisao.js
+++ dist/revisao.js
@@ -1,8 +1,10 @@
 var n1 = 10;
 var n2 = 9;
-var n3 = 5;
-var fmedia = function (n1, n2, n3) { return (n1 + n2 + n3) / 3; };
-console.log("A media \u00E9 : " + fmedia(n1, n2, n3));
+var fmedia = function (n1, n2, n3) {
+    if (n3 === void 0) { n3 = 7; }
+    return (n1 + n2 + n3) / 3;
+};
+console.log("A media \u00E9 : " + fmedia(n1, n2));
 //if(media >=7){
 //  console.log("Aprovado " + fmedia)
 //}
--- revisao.ts
+++ revisao.ts
@@ -1,17 +1,16 @@
 let n1: number = 10
 let n2: number = 9 
-let n3: number = 5
 
 
-let fmedia = (n1:number,n2:number,n3:number )  =>  (n1 + n2 + n3)/3
-
-
+let fmedia = (n1:number,n2:number,n3:number=7 )  =>  (n1 + n2 + n3)/3
 
 
 
 
 
-console.log(`A media é : ${fmedia(n1,n2,n3)}`)
+
+
+console.log(`A media é : ${fmedia(n1,n2)}`)
 
 
 
