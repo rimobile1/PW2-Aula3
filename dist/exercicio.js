3fcf20c00bb15cb21959f8c7d0f2cdbf63e127ed
rimobile1 <icaro.mo1234@gmail.com>
2019-08-14 22:26:57 -0300

    Exercicio de calculo de volume

---
+++ dist/exercicio.js
@@ -0,0 +1,5 @@
+var comp = 10;
+var larg = 9;
+var alt = 5;
+var vmedia = function (comp, larg, alt) { return (comp * larg * alt); };
+console.log("A media \u00E9 : " + vmedia(comp, larg, alt));
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+let comp: number = 10
+let larg: number = 9 
+let alt:  number = 5
+
+
+let vmedia = (comp:number,larg:number,alt:number )  =>  (comp * larg * alt)
+
+
+console.log(`A media é : ${vmedia(comp,larg,alt)}`)
